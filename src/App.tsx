7bcbf3c68d5814fc30dc6aa5694a2c3a5383fc1d
shadow <lightshadow416@gmail.com>
2023-09-29 15:54:10 +0100

    challenge complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import "./style/input.css";
 import "./App.css";
+// components
+import { Item } from "./components";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
+  let apiUrl = "https://fakestoreapi.com/products";
+
+  let [items, setItems]: any = useState([]);
+  let data = async () => {
+    let arr = await axios.get(apiUrl).then((res) => {
+      return res.data;
+    });
+    setItems([...arr]);
+  };
+  useEffect(() => {
+    data();
+  }, []);
   return (
     <>
-      <div>sb</div>
+      <div className="flex flex-col items-center mt-20 px-[4rem]">
+        <h1 className="text-[4rem] font-semibold mb-10">Fake Store</h1>
+        <div className="flex justify-between flex-wrap">
+          {items.map((e) => {
+            return (
+              <Item
+                name={e.title}
+                price={e.price}
+                image={e.image}
+                rate={e.rating.rate}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,3 +1,14 @@
-export default function Item() {
-  return <>Item</>;
+export default function Item({ name, price, rate, image }) {
+  return (
+    <>
+      <div className="bg-white w-[20rem] rounded-xl h-[30rem] flex flex-col py-[2rem] px-[2rem] text-[1.2rem] font-medium mb-[2rem]">
+        <img src={image} alt="img" className="h-[16rem] shadow-lg rounded-xl" />
+        <h2 className="my-[1rem]">{name}</h2>
+        <div className="flex justify-between w-full">
+          <div>Price: ${price}</div>
+          <div className="font-normal">Rating: {rate}</div>
+        </div>
+      </div>
+    </>
+  );
 }
